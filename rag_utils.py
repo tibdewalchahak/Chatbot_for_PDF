4072d2a41301a9277f6e5fa3cf35d680a6f3800e
Chahak Tibdewal <tibdewalchahak1264@gmail.com>
2026-01-07 22:54:47 +0530

    Fix Document import from langchain_core

--- rag_utils.py
+++ rag_utils.py
@@ -2,7 +2,7 @@ import os
 from dotenv import load_dotenv
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
-from langchain.schema import Document
+from langchain_core.documents import Document
 import groq
 
 load_dotenv()
